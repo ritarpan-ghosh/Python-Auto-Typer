624499f1ea42578b2bc5d8867b70a62c1e9773e8
Ritarpan Ghosh <96187334+ritarpan-ghosh@users.noreply.github.com>
2022-10-14 09:28:38 +0530

    The main project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Auto Typing using Python
+
+Run the program, follow the instructions and place the cursor in the right place under required seconds.
---
+++ main.py
@@ -0,0 +1,17 @@
+from winreg import HKEY_LOCAL_MACHINE
+import pyautogui
+import time
+
+msg = input("Enter the massage: ")
+times = int(input("Enter how many times you want to repeate: "))
+new_line = input("Do you want a new line of enter after every line ? (y/n): ")
+print("You have only 10 seconds left to place the cursor.")
+
+num = 0
+time.sleep(5)
+while(num<times):
+    pyautogui.PAUSE = 0.01
+    pyautogui.write(msg, interval=0.15)
+    if new_line == 'y': 
+        pyautogui.press('enter')
+    num = num + 1
